14c3b72847976766bdb671d3aba31587bd8499fc
Katrina Hernandez <abqkatrina@gmail.com>
2020-01-16 19:36:29 -0700

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,7 @@
            https://api.github.com/users/<your name>
 */
 
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +25,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -43,13 +44,86 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+/* List of LS Instructors Github username's: */
+// const friends = {
+//   'abqkatrina',
+//   'tetondan',
+//   'dustinmyes',
+//   'justsml',
+//   'luishrd',
+//   'bigknell',
+// };
+
+
+function CreateCard(response){
+  const card = document.createElement('div');
+
+  const pic = document.createElement('img');
+  const info = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const locus = document.createElement('p');
+  const profile = document.createElement('p');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+   card.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username'); 
+  pic.src = (response.avatar_url);
+
+  name.textContent = ('Name: ' + response.name);
+  username.textContent = ('username: ' + response.login);
+  locus.textContent = ('location: ' + response.location);
+  profile.textContent = ('profile: ' + response.html_url);
+  followers.textContent = ('followers: ' + response.followers);
+  following.textContent = ('following: ' + response.following);
+  bio.textContent = ('bio: ' + response.bio);
+
+  card.appendChild(pic);
+  card.appendChild(info);
+  info.appendChild(name);
+  info.appendChild(username);
+  info.appendChild(locus);
+  info.appendChild(profile);
+  info.appendChild(followers);
+  info.appendChild(following);
+  info.appendChild(bio);
+
+  return card;
+}
+
+const wall = document.querySelector('.cards');
+
+
+const peeps = {
+  abqkatrina: 'https://api.github.com/users/abqkatrina',
+  tetondan:'https://api.github.com/users/tetondan',
+  justsml:'https://api.github.com/users/justsml',
+  luishrd:'https://api.github.com/users/luishrd',
+  bigknell:'https://api.github.com/users/bigknell',
+  keyeric: 'https://api.github.com/users/Keyeric',
+  mileywright: 'https://api.github.com/users/MileyWright',
+  steven_matos: 'https://api.github.com/users/steven-matos',
+  alecdye: 'https://api.github.com/users/alecdye'
+}
+
+const x = Object.values(peeps);
+console.log(x);
+
+x.forEach((i) =>{
+
+axios.get(i)
+
+  .then(response =>{
+    console.log(response.data);
+    wall.append(CreateCard(response.data));
+  })
+  .catch(error => {
+    console.log("error, yo", error);
+});
+});
--- README.md
+++ README.md
@@ -18,12 +18,12 @@ In this project we are going to be accessing the GitHub API and building a socia
 
 **Follow these steps to set up and work on your project:**
 
-* [ ] Create a forked copy of this project.
-* [ ] Add your project manager as collaborator on Github.
-* [ ] Clone your OWN version of the repository (Not Lambda's by mistake!).
-* [ ] Create a new branch: git checkout -b `<firstName-lastName>`.
-* [ ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-* [ ] Push commits: git push origin `<firstName-lastName>`.
+* [X] Create a forked copy of this project.
+* [X] Add your project manager as collaborator on Github.
+* [X] Clone your OWN version of the repository (Not Lambda's by mistake!).
+* [X] Create a new branch: git checkout -b `<firstName-lastName>`.
+* [X] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
+* [X] Push commits: git push origin `<firstName-lastName>`.
 
 **Follow these steps for completing your project.**
 
@@ -36,7 +36,7 @@ In this project we are going to be accessing the GitHub API and building a socia
   * None. We have gone ahead and taken care of this step for you. Focus on the Javascript 😁.
 
 ### Part 1: Requesting Data from the GitHub API
-* Include the script element linking the `axios` library in your HTML. If you do not remember the code you can find it here: https://github.com/axios/axios
+X* Include the script element linking the `axios` library in your HTML. If you do not remember the code you can find it here: https://github.com/axios/axios
 * Follow the instructions found in the GitHubCard/index.js file to request data from the GitHub API.
 
 ### Part 2: Create the component function
